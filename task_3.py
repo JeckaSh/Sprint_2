3b78a651febc83eb058fef27877fe1ed0f7a234f
JeckaSh <jeckashevelkov@icloud.com>
2026-03-11 20:32:21 +0300

    task 3 solution

---
+++ task_3.py
@@ -0,0 +1,45 @@
+class PointsForPlace:
+    def __init__(self):
+        self.points = 0
+
+    def get_points_for_place(self, place):
+        if place > 100:
+            return 'Баллы начисляются только первым 100 участникам'
+        elif place < 1:
+            return 'Спортсмен не может занять нулевое или отрицательное место'
+        else:
+            self.points = 101 - place
+            return int(self.points)
+
+
+class PointsForMeters:
+    def __init__(self):
+        self.points = 0
+        
+    def get_points_for_meters(self, meters):
+        if meters < 0:
+            return 'Количество метров не может быть отрицательным'
+        else:
+            self.points = meters * 0.5
+            return int(self.points)
+            
+
+class TotalPoints(PointsForPlace, PointsForMeters):
+    def __init__(self):
+        super().__init__()
+
+    def get_total_points(self, place, meters):
+        total = super().get_points_for_place(place) + super().get_points_for_meters(meters)
+        return int(total)
+
+
+points_for_place = PointsForPlace()
+print(points_for_place.get_points_for_place(10))
+
+points_for_meters = PointsForMeters()
+print(points_for_meters.get_points_for_meters(10))
+
+total_points = TotalPoints()
+print(total_points.get_points_for_place(10))
+print(total_points.get_points_for_meters(10))
+print(total_points.get_total_points(100, 10))
